27ca826b1af0448a88a00e5be78618dd3908f775
XiRdnethFr <yg2540@nyu.edu>
2022-11-29 07:30:13 +0800

    Projectile Parry

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -5,19 +5,7 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     public float lifetime;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Enemy")
-        {
-            Destroy(collision.gameObject);
-        }
-    }
     // Update is called once per frame
     void FixedUpdate()
     {
---
+++ Assets/Scripts/AttackRangePlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackRangePlayer : MonoBehaviour
+{
+    public Transform bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Projectile")
+        {
+            Vector3 oldvelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = -oldvelocity;
+        }
+    }
+}
